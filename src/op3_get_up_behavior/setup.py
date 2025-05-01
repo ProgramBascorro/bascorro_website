9df5aee7d590f4b57386965bab76f8e5884d0c36
farhansurii <muhammadfarhansuri@students.undip.ac.id>
2025-05-01 21:50:21 +0700

    updated package, tinggal bascorro_demo

--- src/op3_get_up_behavior/setup.py
+++ src/op3_get_up_behavior/setup.py
@@ -1,31 +1,34 @@
-from setuptools import find_packages, setup
-import os # Add this
-from glob import glob # Add this
+# filepath: /home/farhan/motion_webots/src/op3_get_up_behavior/setup.py
+from setuptools import setup
+import os
+from glob import glob
 
 package_name = 'op3_get_up_behavior'
 
 setup(
     name=package_name,
     version='0.0.1',
-    packages=find_packages(exclude=['test']),
+    packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        # Install the YAML motion script files
-        # Adjust 'resource/motion_scripts' if you put them elsewhere
-        (os.path.join('share', package_name, 'motion_scripts'), glob(os.path.join('resource', 'motion_scripts', '*.yaml'))),
+        # Sertakan file launch jika ada
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        # Sertakan file config jika ada
+        # (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='Your Name',
     maintainer_email='your@email.com',
-    description='Get-up behavior node using YAML scripts',
-    license='Apache License 2.0',
-    tests_require=['pytest'],
+    description='Simple get-up behavior node for OP3',
+    license='Apache License 2.0', # Or your license
+    # HAPUS BARIS INI ATAU BARIS SERUPA:
+    # tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'get_up_node = op3_get_up_behavior.get_up_node:main',
+            'get_up_behavior = op3_get_up_behavior.get_up_behavior:main',
         ],
     },
 )
