5d2d44eaca2f3ed7db43a52a8d24ae80b67dcb2f
Program <motionbascorro@gmail.com>
2026-02-19 23:04:37 +0700

    feat: change azzarine image

--- docs/src/lib/team-image-manifest.ts
+++ docs/src/lib/team-image-manifest.ts
@@ -33,7 +33,7 @@ export const TEAM_IMAGE_EXTENSION_BY_YEAR: Record<number, Record<string, string>
     "1r9-TxXtNRV8HRkZGHh3VUAie3OodSAVP": ".jpg",
     "1s0_6_9B0i5HemZz_3jTTJefzY2tjVxLP": ".jpg",
     "1s22-GmFVArFhA9SR8ue3ORVyG2geJVV_": ".png",
-    "1sK83yGgm_G_1VyPWcgBwZ_-TvVvC3NhU": ".jpg",
+    "1sK83yGgm_G_1VyPWcgBwZ_-TvVvC3NhU": ".jpeg",
     "1uGBolc1pt4wNQ2Q6UFGQz7mTODB9VcXr": ".jpg",
     "1wp4P62Ik63xWFnjwsjABfGWpAk33VmVS": ".jpg",
     "1wnuorQxrsgmv2y5z14G_qNJ2ks_1IScO": ".jpg",
